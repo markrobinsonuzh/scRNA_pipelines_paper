3820efb0dfc4bed8b20f1cc00c7ba8584ed797a1
Pierre-Luc Germain <pl.germain@gmail.com>
2020-02-27 18:04:43 +0100

    added old functions to plot old filtering results

--- misc_functions.R
+++ misc_functions.R
@@ -77,6 +77,13 @@ reduceMethods <- function(x, pos=1:2, FUN=mean, sep=" "){
   row.names(x) <- x[,1]
   x[,-1]
 }
+renameNorm <- function(x){
+  pipeComp:::.renameHrows(x, f=function(x){
+    x <- gsub("^norm\\.", "", x)
+    x <- gsub("seuratvst","sctransform",x)
+    x <- gsub(".noscale", " (no scaling)", x)
+  })
+}
 
 cast2 <- function(x, fun.aggregate=median, ...){
   x <- reshape2::dcast(x, fun.aggregate=fun.aggregate, na.rm=T, ...)
@@ -184,4 +191,142 @@ plotBadCells <- function(sce, mis, return_list=FALSE, aspect.ratio=NULL, name=""
   )
   if(return_list) return(pl)
   plot_grid(plotlist = pl, labels=c("",labels[3:7]))
+}
+
+
+
+#' cumVE2
+#'
+#' Plots the proportion of the cumulative variance/deviance explained by clusters, according 
+#' to various gene rankings.
+#'
+#' @param preds A list of names variability measurements, or a data.frame of gene metadata.
+#' @param ve The variance explained by each gene. If NULL, will attempt to read "vst.varExp"
+#' from `preds`.
+#' @param ve.name The name of the measure, default "variance"
+#'
+#' @return a ggplot
+#' @export
+cumVE2 <- function(preds=NULL, ve=NULL, ve.name="variance"){
+  if(is.data.frame(preds) | is(preds,"DataFrame")){
+    ## assume output of `varExplainedByClusters`
+    if(is.null(ve)) ve <- preds[["vst.varExp"]]
+    preds <- list( deviance=preds$deviance,
+                   #    ineq=preds$mean.ineq,
+                   seurat.disp=preds$seurat.dispersion,
+                   seurat.disp.std=preds$seurat.dispersion.scaled,
+                   seurat.vst=preds$seurat.variance,
+                   seurat.vst.std=preds$seurat.variance.standardized,
+                   seurat.trans=preds$seurat.res.var,
+                   expr=preds$total_counts
+    )
+    preds <- lapply(preds, as.numeric)
+  }else{
+    if(is.null(ve)) stop("`ve` must be given!")
+  }
+  if(!is.list(preds)) preds <- list(prediction=preds)
+  ve[which(is.na(ve) | ve < 0)] <- 0
+  ideal <- cumsum(ve[order(ve,decreasing=T)])
+  preds <- lapply(preds, ve=ve, FUN=function(x, ve) cumsum(ve[order(x, decreasing=T)])/ideal)
+  .cumVEsub(preds, ve, cumve.lab=paste("Proportion of the cumulative ",ve.name," explained by clusters"))
+}
+
+
+.cumVEsub <- function(preds, ve, ideal=NULL, cumve.lab=""){
+  df <- data.frame( rank=unlist(lapply(preds,FUN=function(x) 1:length(x))), 
+                    prediction=rep(names(preds), sapply(preds, length)), 
+                    method=rep(gsub("\\.std","",names(preds)), sapply(preds, length)), 
+                    standardized=rep(grepl("std|trans",names(preds)), sapply(preds, length)),
+                    cumVE=unlist(preds) )
+  p <- ggplot(df, aes(x=rank, y=cumVE, colour=method, linetype=standardized)) + geom_line(size=1.5)
+  if(!is.null(ideal)) p <- p + geom_line(data=data.frame(rank=1:length(ve), VE=ideal), aes(x=rank, y=VE), size=1, colour="black", linetype=2)
+  p + ylab(cumve.lab) + xlab("Gene rank")
+}
+
+
+
+
+old_mergeFilterOut <- function(ll){
+  if(length(ll)==1) return(ll[[1]])
+  # get back the initial number of cells
+  N <- attr(ll[[1]],"initialNumbers")
+  if(is.null(N)){
+    x <- abs(as.matrix(ll[[1]]))
+    pc <- x[,grep("^pcOut",colnames(x))]
+    n <- x[,grep("^nOut",colnames(x))]
+    N <- matrixStats::colMedians(round(100*n/pc), na.rm = TRUE)
+    N[is.na(N)] <- 100
+  }
+  ll2 <- lapply(ll, FUN=function(x){
+    x <- x[,grep("^nOut",colnames(x))]
+    attr(x, "initialNumbers") <- NULL
+    cbind(parsePipNames(row.names(x)), x)
+  })
+  x <- rev(ll2)[[1]]
+  for(i in seq.int(length(ll)-1,1)){
+    y <- ll2[[i]]
+    of <- intersect(colnames(x),colnames(y))
+    of <- of[grep("^nOut", of, invert=TRUE)]
+    p1 <- apply(x[,of,drop=FALSE],1,FUN=function(x) 
+      paste(paste0(of,"=",x), collapse=";"))
+    y <- y[,grep("^nOut", colnames(y))]
+    row.names(y) <- apply(ll2[[i]][,of,drop=FALSE],1,FUN=function(x) 
+      paste(paste0(of,"=",x), collapse=";"))
+    colnames(y) <- NULL
+    x <- x[,grep("^nOut", colnames(x))]
+    x+y[p1,]
+  }
+  pc <- round(100*t(t(x)/N),3)
+  pc[pc>100] <- 100
+  colnames(pc) <- gsub("^nOut","pcOut", colnames(pc))
+  cbind(x, pc, deparse.level=0)
+}
+
+
+
+#' old scrna_evalPlot_filtering
+#'
+#' @param res Aggregated pipeline results (i.e. the output of `runPipeline` or
+#' `aggregateResults`)
+#' @param steps Steps to include (default 'doublet' and 'filtering'); other 
+#' steps will be averaged.
+#' @param returnTable Logical; whether to return the data.frame rather than plot.
+#'
+#' @return A ggplot, or a data.frame if `returnTable=TRUE`
+#' @export
+old_evalPlot_filtering <- function(res, steps=c("doublet","filtering"), returnTable=FALSE){
+  library(ggplot2)
+  co <- old_mergeFilterOut(res[steps])
+  co <- abs(as.matrix(co[,grep("^pcOut",colnames(co))]))
+  ds <- sapply(strsplit(colnames(co),"\\."), FUN=function(x) x[2])
+  ds <- split(seq_len(ncol(co)), ds)
+  df <- data.frame(
+    method=rep(row.names(co), length(ds)),
+    dataset=rep(names(ds), each=nrow(co)),
+    maxPCout=unlist(lapply(ds, FUN=function(x) rowMaxs(co[,x]))),
+    medianPCout=unlist(lapply(ds, FUN=function(x) rowMedians(co[,x])))
+  )
+  ds <- sapply(strsplit(colnames(res$clustering)," "), FUN=function(x) x[1])
+  ds <- split(seq_len(ncol(res$clustering)), ds)
+  ds <- ds[grep("stepElapsed",names(ds),invert=TRUE)]
+  ds <- lapply(ds, FUN=function(x){
+    cl <- res$clustering[,x]
+    by <- sapply(strsplit(row.names(cl),";"), FUN=function(x) paste(x[1:2],collapse=";"))
+    meanF1 <- aggregate(cl[[grep("mean_F1",colnames(cl))]], by=list(by), na.rm=TRUE, FUN=mean)
+    topF1 <- aggregate(cl[[grep("mean_F1",colnames(cl))]], by=list(by), na.rm=TRUE, FUN=max)
+    w <- which(cl[[grep("n_clus",colnames(cl))]]==cl[[grep("nbClusts",colnames(cl))]])
+    cl <- cl[w,]
+    tmp <- aggregate(cl[[grep("mean_F1",colnames(cl))]], by=list(by[w]), na.rm=TRUE, FUN=mean)
+    F1atK <- tmp[,2]
+    names(F1atK) <- tmp[,1]
+    d <- data.frame(method=meanF1[,1], meanF1=as.numeric(meanF1[,2]), 
+                    topF1=as.numeric(topF1[,2]), F1atK=F1atK[as.character(meanF1[,1])])
+    d
+  })
+  ds2 <- dplyr::bind_rows(ds,.id="dataset")
+  m <- merge(df, ds2, by=c("dataset","method"))
+  if(returnTable) return(m)
+  ggplot(m, aes(maxPCout, meanF1, group=method, colour=method)) + geom_point(size=3) + 
+    facet_wrap(~dataset, scales="free") + xlab("Proportion") + 
+    xlab("Max proportion of subpopulation excluded")
 }
